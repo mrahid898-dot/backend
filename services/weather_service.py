e69532b2164c8aaf6c44bfa2c42cda2287b2550d
mrahid898-dot <mrahid898@gmail.com>
2026-08-29 00:49:09 +0530

    Create weather_service.py

---
+++ services/weather_service.py
@@ -0,0 +1,2 @@
+async def get_weather(city):
+ return {'city':city.title(),'country':'IN','temperature':31.0,'feels_like':34.0,'humidity':74,'wind_speed':14.0,'condition':'Clouds','description':'partly cloudy','demo':True}
